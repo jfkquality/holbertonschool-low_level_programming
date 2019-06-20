aa7421f613ec7c95d3a9e769e16ce9c48f30815b
JFK <jfkquality@gmail.com>
2019-06-20 18:45:52 +0000

    Debug #5 - printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - print line with printf
+ * Description: Write a C program that prints exactly
+ * (with proper grammar, but the outcome is a piece of art,),
+ * followed by a new line.
+ * Return: 0
+ */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-  return(0);
+	return (0);
 }
