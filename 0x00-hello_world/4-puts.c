467424d6c597c1be20e48c2eb28fb239d2bed5ea
JFK <jfkquality@gmail.com>
2019-06-20 18:25:16 +0000

    Add comment block to 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * print a line showing an opening doublquite;
+ * but not closing double quote;
+ * followed by a newline.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
