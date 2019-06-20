544e77e18d8fb7827a5806351e53a36b88979f8f
JFK <jfkquality@gmail.com>
2019-06-20 20:58:34 +0000

    Add 'byte(s)' to the end of each output line

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	long long ll;
 	float f;
 
-	printf("Size of a char: %lu\n", sizeof(c));
-	printf("Size of an int: %lu\n", sizeof(i));
-	printf("Size of a long int: %lu\n", sizeof(l));
-	printf("Size of a long long intr: %lu\n", sizeof(ll));
-	printf("Size of a float: %lu\n", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+	printf("Size of a long long intr: %lu byte(s)\n", sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 
 	return (0);
 }
