6013ae364e94e96c71cde3289658ea234c280021
JFK <983@holbertonschool.com>
2019-06-22 04:08:00 +0000

    Debug to make it linter compliant

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - print text to stderr
+ * Description: print text to stderr
+ * Return: 1
+ */
 int main(void)
 {
-  fprintf (stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" \
+	- Dora Korpar, 2015-10-19\n");
 
-  return (1);
+	return (1);
 }
