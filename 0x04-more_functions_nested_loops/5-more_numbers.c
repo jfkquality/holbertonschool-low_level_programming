080baef21cbb014f85ddfa15c2aae8c5f15ff243
JFK <jfkquality@gmail.com>
2019-06-27 23:44:41 +0000

    Print 0-14 10 times init commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print number 0-9
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			{
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+			}
+			else
+			{
+				_putchar(j + '0');
+			}
+		}
+		j = 0;
+		_putchar('\n');
+	}
+}
