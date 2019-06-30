b27f552a81b89ea2df5ed633bfb20f2e6fb83de9
JFK <983@holbertonschool.com>
2019-06-30 01:05:42 +0000

    put fucntions in the same file for 0x04, print integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
 
+/*
+int digits(int);
+void print_number2 (int, int, int)
+*/
+
 
 /*
  * print_number - print any integer
@@ -33,4 +38,55 @@ void print_number(int n)
 	print_number2(n, c, tens);
 
 
+}
+
+
+void print_number2 (int num, int count, int ten)
+{
+  int c = count;
+  int d, x = 0;
+  int n = num;
+  int tens = ten;
+
+  int digits(int a);
+
+  while (c > 0)
+    {
+      x = n;
+      n = n / tens; /* Leading digit */
+      if (x > 9)
+	x = x % (n * tens); /* Remaining number(s). For embedded 0's */
+      d = digits(x);
+      if (n == 0) /* print last digit */
+	_putchar(x % 10 + '0');
+      else
+	{
+	  _putchar(n + '0');
+	  while (c - 1 - d) /* Print embedded zeros */
+	    {
+	      _putchar(0 + '0');
+	      c--;
+	      if (tens > 10)
+		tens /= 10;
+	    }
+	}
+      c--;
+      if (tens > 10)
+	tens /= 10;
+      n = x;
+    }
+  _putchar('\n');
+}
+
+
+int digits(int n)
+{
+  int totalDigits = 0;
+
+  while(n != 0)
+    {
+      n = n / 10;
+      totalDigits ++;
+    }
+  return (totalDigits);
 }
