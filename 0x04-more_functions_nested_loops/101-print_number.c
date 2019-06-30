168b927d1fca422114a4d6b46330e94d9b3eabe9
JFK <983@holbertonschool.com>
2019-06-30 01:38:14 +0000

    Combine functions in one file to pass QA Review. Make betty compliant.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,17 +1,10 @@
 #include "holberton.h"
 
-/*
-int digits(int);
-void print_number2 (int, int, int)
-*/
-
-
-/*
+/**
  * print_number - print any integer
  * @n: - integer to print
  * Return: 0
  */
-
 void print_number(int n)
 {
 	int c, b = 0;
@@ -41,52 +34,64 @@ void print_number(int n)
 }
 
 
-void print_number2 (int num, int count, int ten)
+/**
+ * print_number2 - print any integer
+ * @num: integer to print
+ * @count: count of integers
+ * @ten: 10 to the pwoer of count
+ * Return: 0
+ */
+void print_number2(int num, int count, int ten)
 {
-  int c = count;
-  int d, x = 0;
-  int n = num;
-  int tens = ten;
+	int c = count;
+	int d, x = 0;
+	int n = num;
+	int tens = ten;
 
-  int digits(int a);
+	int digits(int a);
 
-  while (c > 0)
-    {
-      x = n;
-      n = n / tens; /* Leading digit */
-      if (x > 9)
-	x = x % (n * tens); /* Remaining number(s). For embedded 0's */
-      d = digits(x);
-      if (n == 0) /* print last digit */
-	_putchar(x % 10 + '0');
-      else
+	while (c > 0)
 	{
-	  _putchar(n + '0');
-	  while (c - 1 - d) /* Print embedded zeros */
-	    {
-	      _putchar(0 + '0');
-	      c--;
-	      if (tens > 10)
-		tens /= 10;
-	    }
+		x = n;
+		n = n / tens; /* Leading digit */
+		if (x > 9)
+			x = x % (n * tens); /* Remaining number(s). For embedded 0's */
+		d = digits(x);
+		if (n == 0) /* print last digit */
+			_putchar(x % 10 + '0');
+		else
+		{
+			_putchar(n + '0');
+			while (c - 1 - d) /* Print embedded zeros */
+			{
+				_putchar(0 + '0');
+				c--;
+				if (tens > 10)
+					tens /= 10;
+			}
+		}
+		c--;
+		if (tens > 10)
+			tens /= 10;
+		n = x;
 	}
-      c--;
-      if (tens > 10)
-	tens /= 10;
-      n = x;
-    }
-  _putchar('\n');
+	_putchar('\n');
 }
 
 
+/**
+ * digits - count the digits of a number
+ * @n: integer to print
+ * Return: count of digits
+ */
 int digits(int n)
 {
-  int totalDigits = 0;
+	int totalDigits = 0;
 
-  while(n != 0)
-    {
-      n = n / 10;
-      totalDigits ++;
-    }
-  return (totalDigits);
+	while (n != 0)
+	{
+		n = n / 10;
+		totalDigits++;
+	}
+	return (totalDigits);
 }
