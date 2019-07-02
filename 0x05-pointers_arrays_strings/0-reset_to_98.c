fa05f92f2e03946caf01a162e4283a0b0b36a212
JFK <jfkquality@gmail.com>
2019-07-02 13:39:19 +0000

    Add effing varialbe description for a passed parameter

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
-#include <stdio.h>
+
 
 /**
  * reset_to_98 - set the passed parameter to 98
- * Description: Reset parament to 98 using pointers
- * Return: void.
+ * Description: set parameter to 98 using pointers
+ * @n: passed parameter
+ * Return: void
  */
-void reset_to_98(int *n)
+void  reset_to_98(int *n)
 {
 	*n = 98;
 }
