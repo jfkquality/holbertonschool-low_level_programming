e02c7a27e23bff6ae2fe5b2ae448fddc8dddfe1a
JFK <jfkquality@gmail.com>
2019-07-02 16:23:34 +0000

    Print string in reverse. Init commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_rev  - Print string in reverse
+ * s: passed string as pointer
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
