ec33077d1797b97fd16e00971a8241cd4aa5690c
JFK <jfkquality@gmail.com>
2019-07-02 13:07:02 +0000

    Made 0-reset betty compliant

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 #include <stdio.h>
 
+/*
+ * reset_to_98 - set the passed parameter to 98
+ * Description: Reset parament to 98 using pointers
+ * Return: void.
+ */
 void reset_to_98(int *n)
 {
-/*
-	printf("Value of n: %d\n", n);
-	printf("Address of n: %d\n", &n);
-*/
 	*n = 98;
-
 }
