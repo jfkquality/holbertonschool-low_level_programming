53b38e5771e999ab899d8bb728c4acb1bf1f7575
JFK <983@holbertonschool.com>
2019-07-03 00:31:32 +0000

    Debug 6-puts.c to work for all lengths

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,20 @@
  */
 void puts2(char *s)
 {
+	char *s1 = s;
 	char *s2 = s;
+	int i = 0;
 
 	while (*s2 != '\0')
 	{
-		_putchar(*s2);
-		s2 += 2;
+		s2++;
+		i++;
+	}
+
+	while (s1 <= s2)
+	{
+		_putchar(*(s1));
+		s1 += 2;
 	}
 	_putchar('\n');
 }
