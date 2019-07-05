a3d825fc18cd89591ad1ae21e2ec708c84c6a1a1
JFK <jfkquality@gmail.com>
2019-07-05 20:05:07 +0000

    0x06, 0-2. Init commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcpt - Copy a src string to a dest string
+ * @dest: string pointer parameter
+ * @src: string pointer parament
+ * Return: dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *dest2 = dest;
+	char *src2 = src;
+	int i = 0;
+
+	for (i = 0; i < n && *src2 != '\0'; i++, src2++, dest2++)
+		*dest2 = *src2;
+	/* *dest2 += '\0'; */
+	for ( ; i < n; i++, dest2++)
+		*dest2 = '\0';
+	return (dest);
+}
