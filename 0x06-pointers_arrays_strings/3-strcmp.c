3da7ec834c3e175789ce66f222c63d2af442036c
JFK <983@holbertonschool.com>
2019-07-08 03:08:58 +0000

    0x06 #3 - Subtract VALUES NOT MEMORY ADDRESSES!

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,24 +9,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	/* int c1 = 0; */
-	/* int c2 = 0; */
 	char *src1 = s1;
 	char *src2 = s2;
-	int diff = 0;
-	/* char *src1a = s1; */
-	/* char *src2a = s2; */
 
 	for (; *src1 != '\0' && *src2 != '\0'; src1++, src2++)
 	{
 		if (*src1 != *src2)
-		{
-			diff = src1 - src2;
-			if (src1 < src2)
-				return (++diff);
-			else
-				return (--diff);
-		}
+			return (*src1 - *src2);
 	}
 	return (0);
 }
