98e6c1dfbc0818f184100de58df0d143f7f1b0c1
JFK <983@holbertonschool.com>
2019-07-08 02:32:37 +0000

    Not sure I committed the last changes?

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,25 +9,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int c1 = 0;
-	int c2 = 0;
+	/* int c1 = 0; */
+	/* int c2 = 0; */
 	char *src1 = s1;
 	char *src2 = s2;
+	/* char *src1a = s1; */
+	/* char *src2a = s2; */
 
-	for (; *src1 != '\0' && *src2 != '\0'; src1++, srr2++, c1++)
-		if (
-		;
-
-	for (; *src1 != '\0'; src1++, c1++)
-		;
-
-	for (; *src2 != '\0'; src2++, c2++)
-		;
-
-	if (s1 < s2)
-		return (-15);
-	else if (s1 > s2)
-		return (15);
-	else
-		return (0);
+	for (; *src1 != '\0' && *src2 != '\0'; src1++, src2++)
+	{
+		if (*src1 != *src2)
+		{
+			return src1 - src2;
+		}
+	}
+	return (0);
 }
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,5 +10,11 @@ char *cap_string(char *str)
 {
 	char *s1 = str;
 
-
+	while (*s1 != '\0')
+	{
+		if (*s1 >= 'a' && *s1 <= 'z')
+			*s1 = *s1 - 32;
+		s1++;
+	}
+	return (str);
 }
