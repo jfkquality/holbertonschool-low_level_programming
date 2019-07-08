6927b3c95d17c3cfd01d1fda49b28af7ad21a7a2
JFK <jfkquality@gmail.com>
2019-07-08 21:00:53 +0000

    Make 0x06 #6 betty compliant

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,31 +11,38 @@ int is_separator(char *);
 char *cap_string(char *str)
 {
   /**
-   * 1. If the first char is a let, capitalize it if it's not (i.e. if it's between a-z then -32).
+   * 1. If the first char is a let, capitalize it if it's not;
+   * (i.e. if it's between a-z then -32).
    * 2. Continue until separator found.
-   * 3. Capitalize the next letter if it's not (i.e. if it's between a-z then -32) 
-   * 4. Repeat from step 2 until end of source.
+   * 3. Repeat from step 2 until end of source.
   */
 	char *s1 = str;
 
 	while (*s1)
 	{
-	/* If it's the first char in str, or the char before is a separator, cap it */
+	/**
+	 * If it's the first char in str,
+	 * or the char before is a separator, cap it.
+	 */
 		if (s1 == str || is_separator(s1 - 1))
 		{
-		    if (*s1 >= 'a' && *s1 <= 'z')
-			    *s1 = *s1 - 'a' + 'A';
+			if (*s1 >= 'a' && *s1 <= 'z')
+				*s1 = *s1 - 'a' + 'A';
 		}
 		s1++;
 	}
 	return (str);
 }
-
-
+/**
+ * is_separator  - find separators in string
+ * @c: character pointer to test
+ * Return: string
+ */
 int is_separator(char *c)
 {
 	char separators[] = "' '\t\n,;.!?\"(){}";
 	char *sep2 = separators;
+
 	while (*sep2)
 	{
 		if (*c == *sep2)
