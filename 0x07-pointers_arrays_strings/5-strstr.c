9f0d273833fcdd6a35f3f14e7b00a9ae0111ad7d
JFK <983@holbertonschool.com>
2019-07-11 01:37:31 +0000

    0x07 #5 strstr. Init commit.

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * _strstr - finc sub string in a string
+ * _strstr - find sub string in a string
  * @haystack: pointer to string to search
  * @needle: chars to find
  *
@@ -12,62 +13,36 @@ char *_strstr(char *haystack, char *needle)
 	char *hay1 = haystack;
 	char *hay2 = haystack;
 	char *need1 = needle;
-	char need2 = needle;
-	char *hay_start;
+	char *need2 = needle;
 	int size_h = 0;
 	int size_n = 0;
-	int hay_posn_c = 0;
-	boolean found = 1;
 
 	/* Find len of both strings */
-
 	for (; *hay1; hay1++, size_h++)
 		;
 	for (; *need1; need1++, size_n++)
 		;
 
-	/* hay1 = haystack; */
-	/* need1 = needle; */
+	/* If you reach the end of the search string, you’ve found it! ‘ */
+	/* Otw, look again until the end of the original string */
+	/* or too close to the end for the search string to fit. */
 
-	while (found && *hay2 && size_h - hay_posn_c <= size_n)
+	/* If not found or at end or almost end of str*/
+	while (hay2 < hay1 - size_n + 1)
 	{
-		if (size_h = hay_posn_c || size_h - hay_posn_c < size_n)
-		{ /* If end or almost end of str*/
-			found = 0;
-			continue;
-		}
+		need2 = needle;
 
-		if (!find_first_let(hay2, need2))
-		{
-			found = 0;
-			continue;
-		}
-		hay2++;
-		hay_posn_c++;
-		need2++;
-		while (#need2) /* See if succeeding letters match substring */
+		while (*need2)
 		{
 			if (*hay2 != *need2)
-			{
-				found = 0;
-				continue;
+				break;
 			hay2++;
 			need2++;
 		}
-	}
-	return (total_c);
-}
-
-int find_first_let(char *h, char *n
-	while (*hay2) /* find the first occurence of the firsr let in substr  */
-	{
-		if (*hay2 == *need2)
-		{
-			break;
-		}
 		hay2++;
-		hay_posn_c++;
-
 	}
-	*hay_start = hay2;
-		return (0);
+		if (!*need1)
+			return (hay2 - size_n - 1);
+		else
+			return (0);
+}
