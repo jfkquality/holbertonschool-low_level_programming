0c0240b0e67ba000bb2c283a449e5d5771fc6752
JFK <983@holbertonschool.com>
2019-07-14 03:32:40 +0000

    0x0A #0. Make betty compliant.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[])
+/**
+ * main - print program name
+ * @argc: count of args
+ * @argv: array of args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
 	return (0);
