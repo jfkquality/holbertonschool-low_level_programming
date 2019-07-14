5d0d0b691693c4404580f16870394aa316a41bb1
JFK <983@holbertonschool.com>
2019-07-14 04:59:42 +0000

    0x0A #3. Init commit.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print program args count
+ * @argc: count of args
+ * @argv: array of args
+ * Return: 0
+ */
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	int i;
+	int res = 1;
+
+	if (argc <= 2)
+	{
+		printf("Error");
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		res *= atoi(argv[i]);
+	}
+	printf("%d\n", res);
+	return (0);
+}
