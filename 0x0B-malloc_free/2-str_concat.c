64b6baa8f785f448648ec770b0ceb0d7a40050c6
JFK <983@holbertonschool.com>
2019-07-17 03:06:27 +0000

    0x0B #2. Init commit.

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - concatenate 2 strings. Return pointer.
+ * @s1: string
+ * @s2: string
+ * Return: pointer to array
+ */
+char *str_concat(char *s1, char *s2)
+{
+
+	int l1 = 0;
+	int l2 = 0;
+	int i = 0;
+	int j = 0;
+	char *str1 = s1;
+	char *str2 = s2;
+	char *p;
+
+	/* if (!str1) */
+	/* return (NULL); */
+
+	for (; *str1; l2++, str1++, l1++)
+		;
+
+	for (; *str2; l2++, str2++, l2++)
+		;
+
+	p = malloc((l1 + l2) * sizeof(char));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < l1; i++)
+		p[i] = s1[i];
+
+	for (j = 0; i < l1 + l2; i++, j++)
+		p[i] = s2[j];
+
+	return (p);
+
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -21,4 +21,5 @@ void print_number2(int, int, int);
 
 char *create_array(unsigned int, char);
 char *_strdup(char *);
+char *str_concat(char *, char *);
 #endif /* HOLBERTON_H */
