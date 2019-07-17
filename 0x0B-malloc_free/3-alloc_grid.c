2c0ffdaf72eeb480425b0bd33b6a2727c2a3af69
JFK <983@holbertonschool.com>
2019-07-17 03:58:56 +0000

    0x0B #3. Init commit. Unfinished.

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * alloc_grid - Return pointer to grid.
+ * @width: width
+ * @sheight: height
+ * Return: pointer to array
+ */
+int **alloc_grid(int width, int height)
+{
+
+	int i = 0;
+	/* int j = 0; */
+	int *p;
+
+	if (!width || !height)
+		return (NULL);
+
+
+	/* printf("l1 and l2 = %d %d\n", l1, l2); */
+
+	p = malloc(width * height * sizeof(int));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (; i < width * height; i++)
+		*(p + i) = 0;
+
+	return (p);
+
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -22,4 +22,5 @@ void print_number2(int, int, int);
 char *create_array(unsigned int, char);
 char *_strdup(char *);
 char *str_concat(char *, char *);
+int **alloc_grid(int, int);
 #endif /* HOLBERTON_H */
