47ce7c7bcedaaf355447aa027dd38df79dda5e54
JFK <983@holbertonschool.com>
2019-07-19 03:44:38 +0000

    0x0C #0, Init commit.

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void *malloc_checked(unsigned int b)
+{
+	char *p;
+
+	p = (malloc(b));
+	if (p == NULL)
+		exit (98);
+
+	return (p);
+}
