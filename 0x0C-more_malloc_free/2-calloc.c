146c5b129e833b69d90fa8bd3b9a97de80147cf3
JFK <983@holbertonschool.com>
2019-07-20 17:56:18 +0000

    0x0C #2, debug. +1 to nmemb, not at end.

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (!size || !nmemb)
 		return (NULL);
 
-	p = malloc((size * nmemb));
+	p = malloc(size * (nmemb + 1));
 
 	if (p == NULL)
 		return (NULL);
