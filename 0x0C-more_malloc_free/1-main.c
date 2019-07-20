48493e49af433395f769892cfae5cfc5bf01c0e4
JFK <983@holbertonschool.com>
2019-07-20 02:10:43 +0000

    0x0C #2, debug. Fix array size to nmemb * size, not just size.

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
   char *concat;
 
-  concat = string_nconcat(NULL, "hello", 2);
+  concat = string_nconcat("Holberton ", "School", 4);
   printf("%s\n", concat);
   free(concat);
   return (0);
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,17 +12,17 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 
 	unsigned int i = 0;
-	char *p = malloc(size * nmemb);
-
+	char *p;
 
 	if (!size || !nmemb)
 		return (NULL);
 
+	p = malloc(size * nmemb);
 
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size * nmemb; i++)
 		p[i] = '0';
 
 	return (p);
