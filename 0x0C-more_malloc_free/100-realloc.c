bc3747d150255f1ed744ae37e44e2fb5ccf2829c
JFK <983@holbertonschool.com>
2019-07-20 04:30:55 +0000

    0x0C #4. Add missed requirements.

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,11 +16,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *p2;
 	unsigned int i;
 
+	if (new_size == old_size)
+		return (ptr);
+
 	p = (malloc(new_size));
 	if (p == NULL)
-		exit(98);
+		return (NULL);
 
 	p2 = ptr;
+
+	if (p2 == NULL)
+		old_size = new_size;
+
+	if (new_size == 0 && p2)
+		return (NULL);
+
 	for (i = 0; i < old_size; i++)
 		p[i] = p2[i];
 	p[i] = '\0';
