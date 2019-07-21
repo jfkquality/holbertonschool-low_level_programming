0cd51ecdf150cafafe8b4f461073feb08f412e32
JFK <983@holbertonschool.com>
2019-07-21 23:30:51 +0000

    Debug 0x0B #3. Change colummn magnet to int from int*.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -33,7 +33,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = malloc(width * sizeof(int *));
+		arr[i] = malloc(width * sizeof(int));
 		if (arr[i] == NULL)
 		{
 			free(arr[i]);
