16156244bc1d21fa5f6e516307499c2455b2483e
JFK <983@holbertonschool.com>
2019-07-21 23:49:19 +0000

    Debug 0x0B #3. Revert free(arr) to just free(arr).

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,8 +27,7 @@ int **alloc_grid(int width, int height)
 	arr = malloc(height * sizeof(int *)); /* rows */
 	if (arr == NULL)
 	{
-		for (i = 0; i < height; i++)
-			free(arr[i]);
+		free(arr);
 		return (NULL);
 	}
 
