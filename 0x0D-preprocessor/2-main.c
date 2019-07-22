c740e197d9dd2c5622f433ce64ebb8aca407618f
JFK <jfkquality@gmail.com>
2019-07-22 19:40:17 +0000

    0x0D #2. Init commit.

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(__attribute__ ((unused)) int argc, __attribute__ ((unused)) char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
