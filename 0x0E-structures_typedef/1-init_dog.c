4230639f2d596baeab0624035bd3304528fd03fe
JFK <jfkquality@gmail.com>
2019-07-22 23:37:13 +0000

    Debug 0x0E #1. Check for null struct.

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,11 +13,14 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	/* d = malloc(sizeof(struct dog)); */
-	/* if (d == NULL) */
-	/* return (NULL); */
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-	/* return (d);  */
+	if (d)
+	{
+		/* d = malloc(sizeof(struct dog)); */
+		/* if (d == NULL) */
+		/* return (NULL); */
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+		/* return (d);  */
+	}
 }
