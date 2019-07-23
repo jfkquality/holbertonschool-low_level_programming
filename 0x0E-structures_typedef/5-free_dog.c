cc7eb564be8b60e08ff8ecac35e11994b38aeb21
JFK <jfkquality@gmail.com>
2019-07-23 01:47:04 +0000

    Debug 0x0E #5. Check for null d.

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,9 +1,16 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * free_dog - Short description
+ * @d: pointer
+ *
+ * Return: void
+ */
 void free_dog(dog_t *d)
 {
 	free(d->name);
 	free(d->owner);
-	free(d);
+	if (d)
+		free(d);
 }
