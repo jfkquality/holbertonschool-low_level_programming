3550c0a4c3a87315f1df68052fb608c736ceade2
JFK <jfkquality@gmail.com>
2019-07-23 01:44:00 +0000

    0x0E #5. Init commit.

--- 0x0E-structures_typedef/5-main.c
+++ 0x0E-structures_typedef/5-main.c
@@ -10,7 +10,8 @@ int main(void)
 {
 	dog_t *my_dog;
 
-	my_dog = new_dog(NULL, 3.5, NULL);
+	my_dog = new_dog("Django", 3.5, "Jay");
 	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+	free_dog(my_dog);
 	return (0);
 }
