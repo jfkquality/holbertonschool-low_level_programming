87b2b4080bb4c5e74e4679fb26cf279725c9db45
JFK <jfkquality@gmail.com>
2019-07-23 01:18:15 +0000

    Debug 0x0E #4. malloc corretly.?

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,16 +7,30 @@
  * @age: Second member
  * @owner: owner
  *
- * Description: Longer description
+ * Return: pointer
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	{
 		dog_t *d;
+
 		d = malloc(sizeof(dog_t));
 		if (d == NULL)
 			return (NULL);
+		d->name = malloc(sizeof(name) + 1);
+		if (!d->name)
+		{
+			free(d);
+			return (NULL);
+		}
+		d->owner = malloc(sizeof(owner) + 1);
+		if (!d->owner)
+		{
+			free(d->name);
+			free(d);
+			return (NULL);
+		}
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
