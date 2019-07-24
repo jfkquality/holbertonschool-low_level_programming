f92a665ed59b62a329bfa775798c8bc70ea856ac
JFK <983@holbertonschool.com>
2019-07-24 21:52:07 +0000

    Debug 0x0F #0. Make 0-print... betty compliant.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+ * print_name - pointer to functions to print a name in lower & uppercase
+ * @name: name of person
+ * @f: point to print function
+ *
+ * Return: Nothing.
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
