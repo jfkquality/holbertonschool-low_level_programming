fa262b050866183a4d2b3866a2c5375b11551de6
JFK <983@holbertonschool.com>
2019-07-24 23:17:58 +0000

    Debug 0x0F #2. Move check for not found outside of if not NULL.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -25,8 +25,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 			if (cmp(array[i]) != 0)
 				break;
 		}
-		if (i == size)
-			return (-1);
 	}
+	if (i == size)
+		return (-1);
 	return (i);
 }
