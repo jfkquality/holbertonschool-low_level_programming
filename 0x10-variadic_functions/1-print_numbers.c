c0d957759bb68307e57e65977f701bc14ac2133b
jfkquality <jfkquality@gmail.com>
2019-07-26 19:30:39 +0000

    0x10 #1. Init commit.

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print  all integer  parameters.
+ * @separator: comma space
+ * @n: number of parameters
+ * @...: variadic parameter
+ * Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n - 1; i++)
+	{
+		if (separator)
+			printf("%d%s", va_arg(ap, int), separator);
+	}
+	printf("%d\n", va_arg(ap, int));
+
+	va_end(ap);
+}
