c626288be09ce16d11dc84e49dfb0196f677cf7d
jfkquality <jfkquality@gmail.com>
2019-08-02 02:18:30 +0000

    0x12 #0. Init commit.

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print linked list
+ * @h: the linked list
+ *
+ * Return: counter
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+	const list_t *current;
+	const list_t *next;
+
+	current = h;
+	next = current->next;
+
+	while (current->next)
+	{
+		if (current->str)
+			(printf("[%u] %s\n", current->len, current->str));
+		else
+			(printf("[%u] %p\n", current->len, current->str));
+
+		i++;
+		current = next;
+	}
+	if (current->str)
+		(printf("[%u] %s\n", current->len, current->str));
+	else
+		(printf("[%u] %p\n", current->len, current->str));
+
+	i++;
+	return (i);
+}
