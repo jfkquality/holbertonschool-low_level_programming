c3cebeafcfbb2c04c947437b3024ada33af5b8d1
jfkquality <jfkquality@gmail.com>
2019-08-02 03:31:06 +0000

    0x12 #0. Refactor like #1.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * print_list - print linked list
+ * print_list - print linked list elements
  * @h: the linked list
  *
  * Return: counter
@@ -10,31 +10,22 @@
 
 size_t print_list(const list_t *h)
 {
+
 	int i = 0;
 	const list_t *current;
-	const list_t *next;
 
 	current = h;
-	next = current->next;
 
-	if (!h)
-		return (-1);
-
-	while (current->next)
+	while (current)
 	{
-		if (current->str)
-			(printf("[%u] %s\n", current->len, current->str));
-		else
-			(printf("[%d] %p\n", 0, current->str));
-
 		i++;
-		current = next;
-	}
-	if (current->str)
-		(printf("[%u] %s\n", current->len, current->str));
-	else
-		(printf("[%u] %p\n", current->len, current->str));
+                if (current->str)
+		  (printf("[%u] %s\n", current->len, current->str));
+                else
+		  (printf("[%d] %p\n", 0, current->str));
 
-	i++;
+		current = current->next;
+	}
+
 	return (i);
 }
---
+++ 0x12-singly_linked_lists/00-print_list.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print linked list
+ * @h: the linked list
+ *
+ * Return: counter
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+	const list_t *current;
+	const list_t *next;
+
+	current = h;
+	next = current->next;
+
+	if (!h)
+		return (-1);
+
+	while (current->next)
+	{
+		if (current->str)
+			(printf("[%u] %s\n", current->len, current->str));
+		else
+			(printf("[%d] %p\n", 0, current->str));
+
+		i++;
+		current = next;
+	}
+	if (current->str)
+		(printf("[%u] %s\n", current->len, current->str));
+	else
+		(printf("[%u] %p\n", current->len, current->str));
+
+	i++;
+	return (i);
+}
