908134bf84030b5c7a5e21bf233a43a4093737c5
jfkquality <jfkquality@gmail.com>
2019-08-03 18:51:17 +0000

    0x12 #3. WASN'T WORKING B/C I COMPILED WRONG CODE!!

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * _strlen - Get the length of a string
+ *
+ * @s: string parament
+ *
+ * Return: c.
+ */
+int _strlen(const char *s)
+{
+	int c = 0;
+	const char *s2;
+
+	s2 = s;
+
+	while (*s2 != '\0')
+	{
+		s2++;
+		c++;
+	}
+
+	return (c);
+}
+
+/**
+ * add_node_end - add nodes to end of list
+ * @head: double point. ???
+ * @str: new string
+ *
+ * Return: pointer to new list
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *current;
+	/* int i = 0; */
+	int len = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = NULL;
+
+	if (*head) /* Then find the last node */
+	{
+		current = (*head);
+		while (current->next)
+		{
+			len++;
+			current = current->next;
+		}
+		current->next = new;
+	}
+	else
+		*head = new;
+
+
+	return (*head);
+}
