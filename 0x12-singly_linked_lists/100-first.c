94c3df4e1c61dc6bab6314a9ac50ad171003dcee
jfkquality <jfkquality@gmail.com>
2019-08-04 01:00:59 +0000

    0x12 #5. Init commit.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * myfirst - print line before main()
+ *
+ * Return: nothing
+ */
+
+void myfirst(void) __attribute__ ((constructor));
+
+void myfirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
