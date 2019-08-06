05d57ea7423ba7bfe68cff5c7056def610be99e7
jfkquality <jfkquality@gmail.com>
2019-08-06 17:48:45 +0000

    0x13 #6. Init commit.

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,24 +5,20 @@
 #include "lists.h"
 
 /**
- * free_listint2 - free a list
+ * pop_listint - free a list
  * @head: double point. ???
  *
  * Return: pointer to new list
  */
-void free_listint2(listint_t **head)
+int pop_listint(listint_t **head)
 {
-	listint_t *next;
+	/* listint_t *next; */
 	listint_t *current;
 
-	if (!**head || !*head)
-		head = NULL;
+	if (!*head)
+		return (0);
+
 	current = *head;
-	while (current)
-	{
-		next = current->next;
-		free(current);
-		current = next;
-	}
-	*head = NULL;
+	*head = current->next;
+	return (current->n);
 }
