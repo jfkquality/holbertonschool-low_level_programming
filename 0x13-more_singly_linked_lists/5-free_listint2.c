c5d718bed083fe19eee96e7d366187fbad69f42f
jfkquality <jfkquality@gmail.com>
2019-08-06 15:23:29 +0000

    0x13 #5. Init commit.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - free a list
+ * @head: double point. ???
+ *
+ * Return: pointer to new list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+	listint_t *current;
+
+	current = *head;
+	while (current)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
