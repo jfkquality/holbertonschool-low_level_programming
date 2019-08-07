9fd40e8394507143aef9054eadbbb13667e4ff52
jfkquality <jfkquality@gmail.com>
2019-08-07 04:25:28 +0000

    Debug 0x13 #10. Trying to get memory freed.

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (index == 0)
 		{
 			*head = current->next;
-			return (current->n);
+			free(current->next);
+			return (1);
 		}
 		/* node = 1; */
 		while (current)
@@ -32,7 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			if (node == index - 1)
 			{
 				current->next = current->next->next;
-				/* free(current-next); */
+				free(current->next);
 				return (1);
 			}
 			node++;
