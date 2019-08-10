26836fcd3e7a00da9735b6c6e8fb4b5bd6e32a50
jfkquality <jfkquality@gmail.com>
2019-08-10 02:20:49 +0000

    Debut 0x14 #1. Separated recursion into its own function per Will's feedback.

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,6 +2,21 @@
 #include <stdio.h>
 
 /**
+ * print_binary2 - print binary from decimal without / or %
+ * @n: decimal number to convert
+ *
+ * Return: nothing
+ */
+void print_binary2(unsigned long int n)
+{
+	if (n != 0)
+	{
+		print_binary2(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+}
+
+/**
  * print_binary - print binary from decimal without / or %
  * @n: decimal number to convert
  *
@@ -10,12 +25,8 @@
 void print_binary(unsigned long int n)
 {
 
-	if (n > 0)
-	{
-		print_binary(n >> 1);
-		_putchar((n & 1) + '0');
-	}
-	/* else */
-	/* _putchar('0'); */
-
+	if (n == 0)
+		_putchar('0');
+	else
+		print_binary2(n);
 }
