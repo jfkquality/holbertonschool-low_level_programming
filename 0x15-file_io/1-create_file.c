f62e0db53a794a8f25349fc2ad907d69aaf678ca
jfkquality <jfkquality@gmail.com>
2019-08-15 01:08:34 +0000

    Debug 0x15 #1. Change open to RDWR from Wr only.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,20 +22,14 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	/* open */
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	/* open write only */
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	/* read */
-	/* count = read(fd, buf, letters); */
-	/* buf[count] = '\0'; */
-
 	while (text_content[count])
-	{
 		count++;
-	}
-	text_content[count] = '\0';
+
 	/* write */
 	wrote = write(fd, text_content, count);
 	if (wrote < 0)
