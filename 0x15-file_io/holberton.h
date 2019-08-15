081de7054cf4311d9a3ff3e997c4a562d47cddc0
jfkquality <jfkquality@gmail.com>
2019-08-15 01:14:07 +0000

    Debug 0x15 #1. GIT ADD WAS F'ING SCREWING UP AND I NEVER ADDED HOLBERTON.H!

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -6,5 +6,6 @@ int _isdigit(int c);
 int _strlen(const char *s);
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
