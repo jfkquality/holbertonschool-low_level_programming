39291d2c2d9e90c729eae37648b97072950de98b
jfkquality <jfkquality@gmail.com>
2019-08-17 02:59:57 +0000

    Debug 0x15 #3. Forgot 'Error' in err msg's!!

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,14 +27,14 @@ int main(int argc, char *argv[])
 	fd1 = open(file1, O_RDONLY, 0664);
 	/* while ((fd1 = open(file1, O_CREAT | O_RDONLY, 0662) == -1) */
 	if (fd1 == -1)
-		dprintf(STDERR_FILENO, "Can't read from file %s\n", file1), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file1), exit(98);
 
 	fd2 = open(file2, O_CREAT | O_EXCL | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 < 0)
 		fd2 = open(file2, O_WRONLY | O_TRUNC);
 	/* while ((fd2 = open(file2, O_CREAT | O_WRONLY | O_TRUNC, 0662) == -1) */
 	if (fd2 == -1)
-		dprintf(STDERR_FILENO, "Can't write to %s\n", file2), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file2), exit(99);
 
 	count = bufsize;
 
@@ -43,13 +43,13 @@ int main(int argc, char *argv[])
 		count = read(fd1, buf, bufsize);
 		wrote = write(fd2, buf, count);
 		if (wrote == -1)
-			dprintf(STDERR_FILENO, "Can't write to %s\n", file2), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file2), exit(99);
 	}
 	closed = (close(fd1));
 	if (closed == -1)
-		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd1), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
 	closed = (close(fd2));
 	if (closed == -1)
-		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd2), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
 	return (wrote);
 }
