a44e16b409420a3f05449fbb61511c7deb477abe
jfkquality <jfkquality@gmail.com>
2019-08-17 03:10:31 +0000

    Debug 0x15 #3. Check if count = -1 in loop.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -41,6 +41,8 @@ int main(int argc, char *argv[])
 	while (count != 0)
 	{
 		count = read(fd1, buf, bufsize);
+		if (count == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file1), exit(98);
 		wrote = write(fd2, buf, count);
 		if (wrote == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file2), exit(99);
