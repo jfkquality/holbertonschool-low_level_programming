424e25de8c74cfbbb101dc2a9b21fc1a5fa575e0
jfkquality <jfkquality@gmail.com>
2019-08-30 02:23:51 +0000

    Correct num of chars printed to match checker.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,12 +11,14 @@ int main(void)
 {
 	char *a1 = "and that piece of art is useful\"";
 	int len1 = strlen("and that piece of art is useful\"");
-	char *a2 = "- Dora Korpar, 2015-10-19\n";
+	char *a2 = " - Dora Korpar, 2015-10-19\n";
 	int len2 = strlen("- Dora Korpar, 2015-10-19");
 
 	write(STDERR_FILENO, a1, len1);
 	write(STDERR_FILENO, a2, len2);
 	write(STDERR_FILENO, "\n", 1);
 
+	/* printf("%li\n", strlen("and that piece of art is useful\" - Dora */
+/* Korpar, 2015-10-19")); */
 	return (1);
 }
