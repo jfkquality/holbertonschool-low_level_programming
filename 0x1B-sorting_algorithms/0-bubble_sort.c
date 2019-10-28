46d263dd14671b1491f5741752e99211d4a695b1
jfkquality <jfkquality@gmail.com>
2019-10-28 18:57:05 +0000

    0x1B, #0. Add check for size < 2. Fix 0-O answers

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -14,21 +14,25 @@ void bubble_sort(int *array, size_t size)
 	size_t i = 0, j = 0 /* , newsize */;
 	int temp = 0;
 
-	/* True bubble sort way */
-	while (i < size - 1)
+
+	if 9size  >= 2)
 	{
-		j = 0;
-		while (j < size - i - 1)
+	/* True bubble sort way */
+		while (i < size - 1)
 		{
-			if (array[j] > array[j + 1])
+			j = 0;
+			while (j < size - i - 1)
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					print_array(array, size);
+				}
+				j++;
 			}
-			j++;
+			i++;
 		}
-		i++;
 	}
 }
