7f42726833a4d2386e8643b4a637ce01fefa0611
jfkquality <jfkquality@gmail.com>
2019-10-28 18:43:44 +0000

    0x1B, #0. Linter errors.

---
+++ 0x1B-sorting_algorithms/0-bubble_sort-jfk.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0 /* , newsize */;
+	int temp = 0;
+
+	/* True bubble sort way */
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size - i - 1)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+ 				/* i = j; */
+				/* continue; */
+				/* break; */
+			}
+			j++;
+		}
+		i++;
+	}
+
+	/* My way. Less loop? But not true bubble? */
+	/* while (i < size) */
+	/* { */
+	/* 	j = i + 1; */
+	/* 	while (j < size) */
+	/* 	{ */
+	/* 		if (array[i] > array[j]) */
+	/* 		{ */
+	/* 			temp = array[i]; */
+	/* 			array[i] = array[j]; */
+	/* 			array[j] = temp; */
+	/* 			print_array(array, size); */
+ 	/* 			/\* i = j; *\/ */
+	/* 			/\* continue; *\/ */
+	/* 			/\* break; *\/ */
+	/* 		} */
+	/* 		j++; */
+	/* 	} */
+	/* 	i++; */
+	/* } */
+
+
+	/* Another way; translated from Python */
+	/* while (size > 1) */
+	/* { */
+	/* 	newsize = 0; */
+	/* 	while (i < size) */
+	/* 	{ */
+	/* 		j = i + 1; */
+	/* 		if (array[i] > array[j]) */
+	/* 		{ */
+	/* 			temp = array[i]; */
+	/* 			array[i] = array[j]; */
+	/* 			array[j] = temp; */
+	/* 			print_array(array, size); */
+	/* 			newsize = i; */
+	/* 			/\* i = j; *\/ */
+	/* 			/\* continue; *\/ */
+	/* 			/\* break; *\/ */
+	/* 		} */
+	/* 		i++; */
+	/* 	} */
+	/* 	size = newsize; */
+	/* 	/\* printf("%lu\n", size); *\/ */
+	/* } */
+}
--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "sort.h"
 
+/**
+ * bubble_sort - bubble sort
+ * @array: list to sort
+ * @size: size (# of elements) of array
+ *
+ * Return: nothing
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i = 0, j = 0 /* , newsize */;
@@ -19,58 +26,9 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
- 				/* i = j; */
-				/* continue; */
-				/* break; */
 			}
 			j++;
 		}
 		i++;
 	}
-
-	/* My way. Less loop? But not true bubble? */
-	/* while (i < size) */
-	/* { */
-	/* 	j = i + 1; */
-	/* 	while (j < size) */
-	/* 	{ */
-	/* 		if (array[i] > array[j]) */
-	/* 		{ */
-	/* 			temp = array[i]; */
-	/* 			array[i] = array[j]; */
-	/* 			array[j] = temp; */
-	/* 			print_array(array, size); */
- 	/* 			/\* i = j; *\/ */
-	/* 			/\* continue; *\/ */
-	/* 			/\* break; *\/ */
-	/* 		} */
-	/* 		j++; */
-	/* 	} */
-	/* 	i++; */
-	/* } */
-
-
-	/* Another way; translated from Python */
-	/* while (size > 1) */
-	/* { */
-	/* 	newsize = 0; */
-	/* 	while (i < size) */
-	/* 	{ */
-	/* 		j = i + 1; */
-	/* 		if (array[i] > array[j]) */
-	/* 		{ */
-	/* 			temp = array[i]; */
-	/* 			array[i] = array[j]; */
-	/* 			array[j] = temp; */
-	/* 			print_array(array, size); */
-	/* 			newsize = i; */
-	/* 			/\* i = j; *\/ */
-	/* 			/\* continue; *\/ */
-	/* 			/\* break; *\/ */
-	/* 		} */
-	/* 		i++; */
-	/* 	} */
-	/* 	size = newsize; */
-	/* 	/\* printf("%lu\n", size); *\/ */
-	/* } */
 }
