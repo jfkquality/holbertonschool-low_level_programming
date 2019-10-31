15e71a92cfc4eb90f635116e781794ebcafa2909
jfkquality <jfkquality@gmail.com>
2019-10-31 03:28:46 +0000

    0x1C. Various unsuccesful updates.

--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -2,46 +2,45 @@
 #include <stdlib.h>
 #include "sort.h"
 
-
-int *swap(int *arr, int idx1, int idx2)
+void *swap(int *arr, int idx1, int idx2)
 {
 	int temp;
 
 	temp = arr[idx1];
 	arr[idx1] = arr[idx2];
 	arr[idx2] = temp;
-	return arr;
-}
+/* 	return arr; */
+/* } */
 
 
-/* algorithm partition(A, lo, hi) is */
-int partition(int *A, size_t lo, size_t hi, size_t size)
-{
-	size_t i, j;
-	int pivot, temp;
+/* /\* algorithm partition(A, lo, hi) is *\/ */
+/* int partition(int *A, size_t lo, size_t hi, size_t size) */
+/* { */
+/* 	size_t i, j; */
+/* 	int pivot, temp; */
 
-	pivot = A[hi];
-	i = lo;
-	for (j = lo; j < hi; j++)
-	{
-		if (A[j] < pivot)
-		{
-			/* swap A[i] with A[j] */
-                        temp = A[i];
-                        A[i] = A[j];
-                        A[j] = temp;
-                        print_array(A, size);
-			i++;
-		}
-	}
-	/* swap A[i] with A[hi]: */
-	temp = A[i];
-	A[i] = A[hi];
-	A[hi] = temp;
-	print_array(A, size);
+/* 	pivot = A[hi]; */
+/* 	i = lo; */
+/* 	for (j = lo; j < hi; j++) */
+/* 	{ */
+/* 		if (A[j] < pivot) */
+/* 		{ */
+/* 			/\* swap A[i] with A[j] *\/ */
+/*                         temp = A[i]; */
+/*                         A[i] = A[j]; */
+/*                         A[j] = temp; */
+/*                         print_array(A, size); */
+/* 			i++; */
+/* 		} */
+/* 	} */
+/* 	/\* swap A[i] with A[hi]: *\/ */
+/* 	temp = A[i]; */
+/* 	A[i] = A[hi]; */
+/* 	A[hi] = temp; */
+/* 	print_array(A, size); */
 
-	return i;
-}
+/* 	return i; */
+/* } */
 
 /**
  * quick_sort - bubble sort
@@ -52,17 +51,48 @@ int partition(int *A, size_t lo, size_t hi, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-	size_t lo = 0, hi = array[size - 1];
-	int *A = array;
-	int p = 0;
+	size_t i, j /* lo = 0, hi = array[size - 1] */;
+	/* int *A = array; */
+	/* int arrlo[size]; */
+	/* int arrhi[size]; */
+	int pivot = 0;
 
-	/* algorithm quicksort(A, lo, hi) is */
-	if (lo < hi)
+
+	if (size > 1)
 	{
-		p = partition(A, lo, hi, size);
-		/* quick_sort(A, lo, p - 1); */
-		/* quick_sort(A, p + 1, hi); */
-		quick_sort(A, lo, p - 1);
-		quick_sort(A, p + 1, hi);
+		pivot = array[size -1]; /* select any element of array */
+		left = array[0]; /* first index of array */
+		right = array[size - 1]; /* last index of array */
+		while (left <= right)
+		{
+			while (array[left] < pivot)
+			{
+				left++;
+				while (array[right] > pivot)
+				{
+					right--;
+					if (left >= right)
+					{
+						swap (array[left], array[right]);
+						left++;
+						right--;
+					}
+				}
+			}
+		}
+		/* quicksort(array from first index to right); */
+		/* quicksort(array from left to last index) */
 	}
 }
+
+
+/* 	/\* algorithm quicksort(A, lo, hi) is *\/ */
+/* 	if (lo < hi) */
+/* 	{ */
+/* 		p = partition(A, lo, hi, size); */
+/* 		/\* quick_sort(A, lo, p - 1); *\/ */
+/* 		/\* quick_sort(A, p + 1, hi); *\/ */
+/* 		quick_sort(A, lo, p - 1); */
+/* 		quick_sort(A, p + 1, hi); */
+/* 	} */
+/* } */
