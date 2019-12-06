57afe1718759208853f36f0dd098510ce5ac7c92
jfkquality <jfkquality@gmail.com>
2019-12-06 18:55:55 +0000

    0x1D, Binary Trees, Task 1. Init commit.

---
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -0,0 +1,44 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ *
+ * @parent: parent node
+ * @value: value to assign in node
+ *
+ * Return: node pointer
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node; /* , *tempnode; */
+
+	if (parent == NULL)
+		return NULL;
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+
+	if (parent->left == NULL)
+	{
+		parent->left = node;
+	}
+	else
+	{
+		/* tempnode = parent->left; */
+		node->parent = parent;
+		node->left = parent->left;
+		/* node->right = tempnode->right; */
+		parent->left->parent = node;
+		parent->left = node;
+
+		/* binary_tree_insert_left(parent->left, value); */
+	}
+	return (node);
+}
