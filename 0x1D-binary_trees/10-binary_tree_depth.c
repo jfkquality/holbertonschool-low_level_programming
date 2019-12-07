d2a294e7abd8cfcc37a701cf410d96e02b190e48
jfkquality <jfkquality@gmail.com>
2019-12-07 01:56:22 +0000

    0x1D, Binary Trees, Tasks 10. Linter err

--- 0x1D-binary_trees/10-binary_tree_depth.c
+++ 0x1D-binary_trees/10-binary_tree_depth.c
@@ -1,13 +1,5 @@
 #include "binary_trees.h"
 
-
-/* * find_depth - measures the depth of a node in a binary tree */
- /* * */
- /* * @tree: pointer to the root node of the tree to traverse */
- /* * */
- /* * Return: nothing */
- /* *\/ */
-
 /**
  * binary_tree_depth - measures the depth of a node in a binary tree
  *
@@ -16,7 +8,6 @@
  * Return: nothing
  */
 
-/* size_t find_depth(const binary_tree_t *tree) */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	int depth = 0;
@@ -24,27 +15,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (tree == NULL || tree->parent == NULL)
 		return (0);
 
-	/* depth = find_depth(tree->parent); */
 	depth = binary_tree_depth(tree->parent) + 1;
-
-	/* depth = (depth + 1); */
 
 	return (depth);
 
 }
-/* /\** */
-/*  * binary_tree_depth - measures the depth of a node in a binary tree */
-/*  * */
-/*  * @tree: pointer to the root node of the tree to traverse */
-/*  * */
-/*  * Return: nothing */
-/*  *\/ */
-
-/* size_t binary_tree_depth(const binary_tree_t *tree) */
-/* { */
-/* 	int depth = 0; */
-
-/* 	depth = find_depth(tree); */
-
-/* 	return (depth - 1); */
-/* } */
