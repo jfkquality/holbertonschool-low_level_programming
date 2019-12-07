355c43c8491b14787a8e5eeead1bfa71af19fb3d
jfkquality <jfkquality@gmail.com>
2019-12-07 06:06:25 +0000

    0x1D, Binary Trees, Tasks 10. Somehow didn't add func to header.

--- 0x1D-binary_trees/10-binary_tree_depth.c
+++ 0x1D-binary_trees/10-binary_tree_depth.c
@@ -10,13 +10,10 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	int depth = 0;
+	/* int depth = 0; */
 
 	if (tree == NULL || tree->parent == NULL)
 		return (0);
 
-	depth = binary_tree_depth(tree->parent) + 1;
-
-	return (depth);
-
+	return (binary_tree_depth(tree->parent) + 1);
 }
