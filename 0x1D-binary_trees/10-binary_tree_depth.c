30f6c08a234398c5632853c5833e71ceafead559
jfkquality <jfkquality@gmail.com>
2019-12-07 01:54:40 +0000

    0x1D, Binary Trees, Tasks 10. Refactor to get last output check

--- 0x1D-binary_trees/10-binary_tree_depth.c
+++ 0x1D-binary_trees/10-binary_tree_depth.c
@@ -1,27 +1,13 @@
 #include "binary_trees.h"
 
-/**
- * find_depth - measures the depth of a node in a binary tree
- *
- * @tree: pointer to the root node of the tree to traverse
- *
- * Return: nothing
- */
 
-size_t find_depth(const binary_tree_t *tree)
-{
-	int depth = 0;
+/* * find_depth - measures the depth of a node in a binary tree */
+ /* * */
+ /* * @tree: pointer to the root node of the tree to traverse */
+ /* * */
+ /* * Return: nothing */
+ /* *\/ */
 
-	if (tree == NULL)
-		return (0);
-
-	depth = find_depth(tree->parent);
-
-	depth = (depth + 1);
-
-	return (depth);
-
-}
 /**
  * binary_tree_depth - measures the depth of a node in a binary tree
  *
@@ -30,11 +16,35 @@ size_t find_depth(const binary_tree_t *tree)
  * Return: nothing
  */
 
+/* size_t find_depth(const binary_tree_t *tree) */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	int depth = 0;
 
-	depth = find_depth(tree);
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
 
-	return (depth - 1);
+	/* depth = find_depth(tree->parent); */
+	depth = binary_tree_depth(tree->parent) + 1;
+
+	/* depth = (depth + 1); */
+
+	return (depth);
+
 }
+/* /\** */
+/*  * binary_tree_depth - measures the depth of a node in a binary tree */
+/*  * */
+/*  * @tree: pointer to the root node of the tree to traverse */
+/*  * */
+/*  * Return: nothing */
+/*  *\/ */
+
+/* size_t binary_tree_depth(const binary_tree_t *tree) */
+/* { */
+/* 	int depth = 0; */
+
+/* 	depth = find_depth(tree); */
+
+/* 	return (depth - 1); */
+/* } */
