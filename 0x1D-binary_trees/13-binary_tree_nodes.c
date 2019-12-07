341d955dd14009daa36f9f5ea990029ad18b62a2
jfkquality <jfkquality@gmail.com>
2019-12-07 02:51:42 +0000

    0x1D, Binary Trees, Tasks 13. Init commit.

---
+++ 0x1D-binary_trees/13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - count the nodes with at least 1 child of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to traverse
+ *
+ * Return: nothing
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	int nodes = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left || tree->right)
+		nodes += 1;
+	/* nodes = binary_tree_nodes(tree); */
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+	return (nodes);
+}
