45db1c4d6cc93e380c387db1d954615830923298
jfkquality <jfkquality@gmail.com>
2020-01-15 20:07:56 +0000

    0x1E, Search Algorithms. Task 0. Linter err; line too long (though it wasnt).

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - search for value in array of ints use Linear search algorithm
+ * linear_search - search for value in array of ints use Linear search algorthm
  *
  * @array: pointer to the first element array to traverse
  * @size: is the number of elements in array
